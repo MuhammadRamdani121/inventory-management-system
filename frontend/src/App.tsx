f7399b56c7a0b6698af74c4d0aaa2599c59aace9
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-03 00:26:28 +0700

    Membuat Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
 import "./index.css";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
 
-export default function app() {
+export default function App() {
   return (
     <div className="text-red-200">
-      <h1>Test</h1>
+      <BrowserRouter>
+        {/* Membuat Router Ke default atau Home */}
+        <NavLink to="./Home">Home</NavLink>
+
+        {/* Membuat Router Ke Dashboard */}
+        <NavLink to="./Dashboard">Dashboard</NavLink>
+        {/* Membuat Router Ke Products */}
+        <NavLink to="./Products">Product</NavLink>
+
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
