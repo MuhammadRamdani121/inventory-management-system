f2ea843eb0de33be178d1ae5e9905655529cc56c
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-05 13:36:06 +0700

    Memberikan isActive ke children function

--- frontend/src/component/Sidebar.tsx
+++ frontend/src/component/Sidebar.tsx
@@ -12,26 +12,46 @@ export default function Sidebar() {
           <NavLink
             to="/"
             className={({ isActive }) =>
-              isActive ? "text-black underline" : "text-black"
+              isActive
+                ? "text-black bg-gray-200 rounded px-3 py-2 "
+                : "text-black"
             }
+            end
           >
-            🏠 Home
+            {({ isActive }) => (
+              <>
+                🏠 <span className={isActive ? "underline" : ""}>Home</span>
+              </>
+            )}
           </NavLink>
           <NavLink
             to="/dashboard"
             className={({ isActive }) =>
-              isActive ? "text-black underline" : "text-black"
+              isActive
+                ? "text-black bg-gray-200 rounded px-3 py-2 "
+                : "text-black"
             }
           >
-            📊 Dashboard
+            {({ isActive }) => (
+              <>
+                📊{" "}
+                <span className={isActive ? "underline" : ""}>Dashboard</span>
+              </>
+            )}
           </NavLink>
           <NavLink
             to="/products"
             className={({ isActive }) =>
-              isActive ? "text-black underline" : "text-black"
+              isActive
+                ? "text-black bg-gray-200 rounded px-3 py-2"
+                : "text-black"
             }
           >
-            📦 Products
+            {({ isActive }) => (
+              <>
+                📦 <span className={isActive ? "underline" : ""}>Products</span>
+              </>
+            )}
           </NavLink>
         </nav>
       </aside>
