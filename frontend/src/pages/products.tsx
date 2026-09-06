52e877c4c647d28f97c660960eb8fb72e97b92ab
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-06 23:43:28 +0700

    Menambahkan Filter Category

--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -24,15 +24,28 @@ const products: Product[] = [
     stock: 5,
     category: "Electronics",
   },
+  {
+    id: 3,
+    name: "Monitor",
+    price: 1500000,
+    stock: 5,
+    category: "PC",
+  },
 ];
 
 export default function Products() {
-  // Menambahkan UseState
+  // Menambahkan UseState Filter
   const [search, setSearch] = useState("");
+  // Menambahkan UseState SetCategory
+  const [category, setCategory] = useState("All");
 
+  // data category
+  const categories = ["All", "Electronics"];
   // Filter Product Berdasarkan nama
-  const filterProducts = products.filter((products) =>
-    products.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+  const filterProducts = products.filter(
+    (product) =>
+      product.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) &&
+      (category === "All" || product.category === category),
   );
   return (
     <div>
@@ -46,6 +59,18 @@ export default function Products() {
         onChange={(event) => setSearch(event.target.value)}
       />
 
+      {/* Menambahkan fitur dropdown category */}
+
+      <select
+        value={category}
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
       <ProductTable products={filterProducts} />
     </div>
   );
