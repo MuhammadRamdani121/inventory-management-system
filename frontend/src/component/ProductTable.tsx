68091a62fdb0d80a4ff041f123d40118557e556a
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-06 21:36:46 +0700

    Menambahkan Fitur Seach

---
+++ frontend/src/component/ProductTable.tsx
@@ -0,0 +1,33 @@
+import type { Product } from "../types/Product";
+
+type ProductTableProps = {
+  products: Product[];
+};
+
+export default function ProductTable({ products }: ProductTableProps) {
+  return (
+    <table className="w-full border-collapse">
+      <thead>
+        <tr className="border-b text-left">
+          <th className="p-3">ID</th>
+          <th className="p-3">Product</th>
+          <th className="p-3">Category</th>
+          <th className="p-3">Stock</th>
+          <th className="p-3">Price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id} className="border-b">
+            <td className="p-3">{product.id}</td>
+            <td className="p-3">{product.name}</td>
+            <td className="p-3">{product.category}</td>
+            <td className="p-3">{product.stock}</td>
+            <td className="p-3">Rp{product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
+import type { Product } from "../types/Product";
+import ProductTable from "../component/ProductTable";
+
+const products: Product[] = [
+  {
+    id: 1,
+    name: "Keyboard",
+    price: 250000,
+    stock: 20,
+    category: "Electronics",
+  },
+  {
+    id: 2,
+    name: "Mouse",
+    price: 150000,
+    stock: 15,
+    category: "Electronics",
+  },
+  {
+    id: 3,
+    name: "Monitor",
+    price: 1500000,
+    stock: 5,
+    category: "Electronics",
+  },
+];
+
 export default function Products() {
+  // Menambahkan UseState
+  const [search, setSearch] = useState("");
+
+  // Filter Product Berdasarkan nama
+  const filterProducts = products.filter((products) =>
+    products.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+  );
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Product</h1>
+
+      {/* Menambahkan Fitur Search Menggunakan UseState */}
+      <input
+        type="text"
+        placeholder="Search Product"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+
+      <ProductTable products={filterProducts} />
     </div>
   );
 }
