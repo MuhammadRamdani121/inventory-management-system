4ee984ca990323f66f07bfd05f26519d36c66ec1
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-09 20:59:40 +0700

    Menambahkan Server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Inventory API Is Running",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
