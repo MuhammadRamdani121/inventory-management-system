42c131b248e399e53d5985ea0baefb431814a0c5
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-10 22:13:23 +0700

    Membuat REST API POST

---
+++ backend/src/routes/productRoutes.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+
+const router = Router();
+
+const products = [
+  {
+    id: 1,
+    name: "Keyboard",
+    price: 250000,
+    stock: 20,
+    category: "Electronics",
+  },
+  {
+    id: 2,
+    name: "Mouse",
+    price: 150000,
+    stock: 15,
+    category: "Electronics",
+  },
+  {
+    id: 3,
+    name: "Monitor",
+    price: 1500000,
+    stock: 5,
+    category: "Electronics",
+  },
+];
+
+router.get("/", (req, res) => {
+  res.json(products);
+});
+
+router.post("/", (req, res) => {
+  const newProduct = {
+    id: products.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category: req.body.category,
+  };
+
+  products.push(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 
+import productRoutes from "./routes/productRoutes.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.json({
-    message: "Inventory API Is Running",
+    message: "Inventory API is running!",
   });
 });
 
